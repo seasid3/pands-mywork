e8a97cd87cfdcf7997cf80a8175fe98121222b33
Orla Woods <orla.woods@gmail.com>
2025-02-27 22:48:23 +0000

    lab 1 week 4 exercises

---
+++ week04-flow/grade.py
@@ -0,0 +1,26 @@
+# grade.py
+# This program reads a student's grade, checks it is valid and 
+# prints out the grade
+# Author: Orla Woods
+
+percentage = float(input("Enter the percentage: "))
+print (f"percentage is {percentage}")
+
+if percentage < 0 or percentage > 100:
+     print ("Please enter a number between 0 and 100")
+
+elif percentage < 40:
+     print ("Fail")
+
+elif percentage < 50:
+     print ("Pass")
+
+elif percentage < 60:
+     print ("Merit 1")
+
+elif percentage < 70:
+     print ("Merit 2")
+
+else: print ("Distinction")
+             
+#End
---
+++ week04-flow/isEven.py
@@ -0,0 +1,57 @@
+# isEven.py
+# a program to ask if a number is even or odd and give even or odd
+# as an output
+# Author: Orla Woods
+
+
+
+''' 
+# first code, enter an integer
+
+number = int(input("enter an integer:"))
+
+if (number % 2) == 0:
+    print(f"{number} is an even number")
+else: 
+    print  (f"{number} is an odd number")
+'''
+
+'''
+# next code enter a float, round this and then see if it is even
+# or odd and output even or odd
+
+number = float(input("enter a number:"))
+
+roundnumber = round(number)
+print (f"Rounded to the nearest whole number this is: {roundnumber}")
+
+if (roundnumber % 2) == 0:
+    print(f"{roundnumber} is an even number")
+else: 
+    print  (f"{roundnumber} is an odd number")
+
+'''
+# this code uses a loop to define that if a condition is not met
+# it will keep asking until the correct condition calls for a break
+
+number = int(input("enter an integer that is less than ten:"))
+
+while number >= 0:
+    anothernumber = int(input("enter another integer that is less than ten:"))
+
+    if anothernumber == -1:
+         break
+
+print("The number IS -1!")
+    
+if (anothernumber % 2) == 0:
+    print(f"{anothernumber} is an even number")
+else: 
+    print  (f"{anothernumber} is an odd number")
+
+'''
+if (number % 2) == 0:
+    print(f"{number} is an even number")
+else: 
+    print  (f"{number} is an odd number")
+'''
